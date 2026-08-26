8363f9b02fbdcda92d55f4277c5a102d2a5cdf0c
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-26 20:11:02 -0300

    feat: adiciona entidade nao encontrada exception

--- src/main/java/dev/joaorooliveira/catalogo_filmes/infra/EntidadeNaoEncontradaException.java
+++ src/main/java/dev/joaorooliveira/catalogo_filmes/infra/EntidadeNaoEncontradaException.java
@@ -1,4 +1,7 @@
 package dev.joaorooliveira.catalogo_filmes.infra;
 
-public class EntidadeNaoEncontradaException {
+public class EntidadeNaoEncontradaException extends RuntimeException {
+    public EntidadeNaoEncontradaException(String message) {
+        super(message);
+    }
 }
