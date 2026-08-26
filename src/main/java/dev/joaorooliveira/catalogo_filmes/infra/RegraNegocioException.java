d2bbe3bae88852bb6967f797496e5884aab5ebe7
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-26 20:09:59 -0300

    feat: adiciona regra negocio exception

--- src/main/java/dev/joaorooliveira/catalogo_filmes/infra/RegraNegocioException.java
+++ src/main/java/dev/joaorooliveira/catalogo_filmes/infra/RegraNegocioException.java
@@ -1,4 +1,8 @@
 package dev.joaorooliveira.catalogo_filmes.infra;
 
-public class RegraNegocioException {
+public class RegraNegocioException extends RuntimeException{
+
+    public RegraNegocioException(String message) {
+        super(message);
+    }
 }
