88654edd2d850313eff85382cae16dc551970ab5
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-26 20:54:01 -0300

    feat: adiciona enum genero tipo

---
+++ src/main/java/dev/joaorooliveira/catalogo_filmes/domain/filme/Filme.java
@@ -0,0 +1,62 @@
+package dev.joaorooliveira.catalogo_filmes.domain.filme.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "filme")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Filme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 255,nullable = false)
+    private String nome;
+
+    private String descricao;
+
+    @Column(nullable = false,length = 50)
+    @Enumerated(EnumType.STRING)
+    private GeneroTipo genero;
+
+    @Column(name = "ano_lancamento")
+    private LocalDate anoLancamento;
+
+    private Integer avaliacao;
+
+    @Column(nullable = false)
+    private boolean assistido = false;
+
+    @Column(nullable = false)
+    private boolean favorito = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diretor_id")
+    private Diretor diretor;
+
+    @CreatedDate
+    @Column(name = "criado_em", nullable = false, updatable = false)
+    private LocalDateTime criadoEm;
+
+    @LastModifiedDate
+    @Column(name = "atualizado_em", nullable = false)
+    private LocalDateTime atualizadoEm;
+
+
+
+}
---
+++ src/main/java/dev/joaorooliveira/catalogo_filmes/domain/filme/enums/GeneroTipo.java
@@ -0,0 +1,23 @@
+package dev.joaorooliveira.catalogo_filmes.domain.filme.enums;
+
+public enum GeneroTipo {
+
+    ACAO,
+    AVENTURA,
+    COMEDIA,
+    DRAMA,
+    FICCAO,
+    FANTASIA,
+    TERROR,
+    SUSPENSE,
+    MISTERIO,
+    ROMANCE,
+    ANIMACAO,
+    DOCUMENTARIO,
+    MUSICAL,
+    CRIME,
+    HISTORIA,
+    GUERRA,
+    WESTERN,
+    BIOGRAFIA
+}
