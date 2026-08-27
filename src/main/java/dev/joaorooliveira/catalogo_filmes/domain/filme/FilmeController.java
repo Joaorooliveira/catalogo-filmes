b5e9b775c0978a7d709f601acf13320232057606
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-27 17:25:02 -0300

    feat(filme-service): adiciona endpoint para buscar filmes

--- src/main/java/dev/joaorooliveira/catalogo_filmes/domain/filme/FilmeController.java
+++ src/main/java/dev/joaorooliveira/catalogo_filmes/domain/filme/FilmeController.java
@@ -1,13 +1,15 @@
 package dev.joaorooliveira.catalogo_filmes.domain.filme;
 
+import dev.joaorooliveira.catalogo_filmes.domain.filme.dto.FilmeFiltroRequestDTO;
 import dev.joaorooliveira.catalogo_filmes.domain.filme.dto.FilmeRequestDTO;
 import dev.joaorooliveira.catalogo_filmes.domain.filme.dto.FilmeResponseDTO;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -32,5 +34,11 @@ public class FilmeController {
                 .buildAndExpand(filmeResponseDTO.id())
                 .toUri();
         return ResponseEntity.created(location).body(filmeResponseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<FilmeResponseDTO>> buscar(@PageableDefault(size = 10) Pageable pageable,
+                                                         FilmeFiltroRequestDTO filtro) {
+        return ResponseEntity.ok(filmeService.buscarFilmes(pageable, filtro));
     }
 }
