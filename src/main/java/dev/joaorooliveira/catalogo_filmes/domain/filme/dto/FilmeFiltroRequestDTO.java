c5c5963c35adf8edba894c6005e3072ffa2137a4
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-27 16:18:04 -0300

    feat: adiciona filme filtro request dto

---
+++ src/main/java/dev/joaorooliveira/catalogo_filmes/domain/filme/dto/FilmeFiltroRequestDTO.java
@@ -0,0 +1,16 @@
+package dev.joaorooliveira.catalogo_filmes.domain.filme.dto;
+
+import dev.joaorooliveira.catalogo_filmes.domain.filme.enums.GeneroTipo;
+
+import java.time.LocalDate;
+
+public record FilmeFiltroRequestDTO(
+        String nome,
+        String descricao,
+        GeneroTipo genero,
+        LocalDate anoLancamento,
+        Integer avaliacao,
+        Boolean assistido,
+        Boolean favorito
+) {
+}
---
+++ src/main/java/dev/joaorooliveira/catalogo_filmes/infra/specification/FilmeSpecification.java
@@ -0,0 +1,4 @@
+package dev.joaorooliveira.catalogo_filmes.infra.specification;
+
+public class FIlmeSpecification {
+}
