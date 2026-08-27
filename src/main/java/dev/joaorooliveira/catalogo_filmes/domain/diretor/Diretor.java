f228ccd670f8304ed3a4c672e136f5f03e5baace
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-26 21:00:08 -0300

    feat: adiciona classe diretor

--- src/main/java/dev/joaorooliveira/catalogo_filmes/domain/diretor/Diretor.java
+++ src/main/java/dev/joaorooliveira/catalogo_filmes/domain/diretor/Diretor.java
@@ -1,4 +1,42 @@
 package dev.joaorooliveira.catalogo_filmes.domain.diretor;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "diretor")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Diretor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,length = 255)
+    private String nome;
+
+    @Past
+    private LocalDate dataNascimento;
+
+    @CreatedDate
+    @Column(nullable = false,name = "criado_em")
+    private LocalDateTime criadoEm;
+
+    @LastModifiedDate
+    @Column(nullable = false,name = "atualizado_em")
+    private LocalDateTime atualizadoEm;
 }
