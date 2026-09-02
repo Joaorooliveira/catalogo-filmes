bfd96da8f31f3f7a94e78bd4ab26ab0df25ea2e1
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-02 20:43:53 -0300

    feat(lista-repository): adiciona metodo para buscar lista pelo titulo

--- src/main/java/dev/joaorooliveira/catalogo_filmes/domain/lista/ListaRepository.java
+++ src/main/java/dev/joaorooliveira/catalogo_filmes/domain/lista/ListaRepository.java
@@ -1,7 +1,14 @@
 package dev.joaorooliveira.catalogo_filmes.domain.lista;
 
+import aj.org.objectweb.asm.commons.Remapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface ListaRepository extends JpaRepository<Lista, Long> , JpaSpecificationExecutor<Lista> {
+    Page<Lista> findByTituloContainingIgnoreCase(
+            String titulo,
+            Pageable pageable
+    );
 }
